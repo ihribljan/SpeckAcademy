cd4afa43d1ffe46f455519c3eab27e7b395f3ea2
Ivan Hribljan <ihribljan@gmail.com>
2020-04-19 22:42:50 +0200

    Last year component override

--- react/my-app/src/components/Last_year/Last_year.js
+++ react/my-app/src/components/Last_year/Last_year.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import '../../App.scss';
 import './Last_year.scss';
-import { Link } from 'react-router-dom';
-
-import Last_year from '../../assets/img/last-year.jpg';
 
 const LastYear = () => {
     return (
-        <section class="SectionLastYear">
-            <div class="SectionLastYear__Inner">
-                <h2>FOI tjedan karijera 2019</h2>
-                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                    classical
-                    Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from
-                    a
-                    Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the
-                    undoubtable source.</p>
-                <Link
-                    to="/last_year">
-                    <figure>
-                        <img src={Last_year} alt="last_year"/>
-                    </figure>
-                </Link>
+        <section className="SectionLastYear">
+            <div className="SectionLastYear__Inner">
+                <h6 className="SectionLastYear__Title">FOI tjedan karijera 2019</h6>
+                <div className="SectionLastYear__Container">
+                    <p className="SectionLastYear__Desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur 
+                    illum voluptatum magnam, unde eum modi dolorum similique earum beatae 
+                    ullam cum, quos accusantium odit optio, neque in cumque quasi doloribus? 
+                    Illo ut facere placeat totam animi illum libero, in perferendis ullam ab 
+                    itaque fuga? Non iusto quis officiis tempora, officia eum provident quo? 
+                    Suscipit vitae tempora aspernatur quos voluptatibus necessitatibus.</p>
+                    <div className="SectionLastYear__Image"></div>
+                </div>
             </div>
         </section>
     );
